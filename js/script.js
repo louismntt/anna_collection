52dffb5475fd0bb871761f2bc118beb17d1357ef
louismnt <louis.manotte@gmail.com>
2023-04-29 21:54:45 +0200

    Update

--- js/script.js
+++ js/script.js
@@ -3,17 +3,19 @@
 const playFirstVideo = function() {
     document.getElementById('section-video1').scrollIntoView();
     const video1 = document.getElementById('video1');
-    video1.play();
-    document.exitFullscreen();
+    video1.play()  
     }
 
 // detect end of video 1, scroll to section-video2 and play video2 
 const video1 = document.getElementById('video1');
 video1.addEventListener('ended', function() {
+    this.webkitExitFullscreen();
     document.getElementById('section-video2').scrollIntoView();
     const video2 = document.getElementById('video2');
-    video2.play();
-    document.exitFullscreen();
+    setTimeout(function(){
+        video2.play();
+    }, 500);    
+
 
     }
 );
@@ -21,10 +23,13 @@ video1.addEventListener('ended', function() {
 // detect end of video 2, scroll to section-video3 and play video3
 const video2 = document.getElementById('video2');
 video2.addEventListener('ended', function() {
+    this.webkitExitFullscreen();
     document.getElementById('section-video3').scrollIntoView();
     const video3 = document.getElementById('video3');
-    video3.play();
-    document.exitFullscreen();
+    setTimeout(function(){
+        video3.play();
+    }, 500);    
+
 
     }
 );
@@ -32,10 +37,13 @@ video2.addEventListener('ended', function() {
 // detect end of video 3, scroll to section-video4 and play video4
 const video3 = document.getElementById('video3');
 video3.addEventListener('ended', function() {
+    this.webkitExitFullscreen();
     document.getElementById('section-video4').scrollIntoView();
     const video4 = document.getElementById('video4');
-    video4.play();
-    document.exitFullscreen();
+    setTimeout(function(){
+        video4.play();
+    }, 500);    
+
 
     }
 );
